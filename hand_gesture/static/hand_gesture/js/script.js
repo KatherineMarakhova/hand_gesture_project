f720289412ba195ad10378ffd7f6fd662ac8b9fa
murka <katherine.marakhova@gmail.com>
2024-12-04 18:38:44 +0300

    добавлено распознавани левой/правой руки, уведомления и переадресация после завершения упражнений

--- hand_gesture/static/hand_gesture/js/script.js
+++ hand_gesture/static/hand_gesture/js/script.js
@@ -1,13 +1,4 @@
-function get_sum(isOpenFingers){
-    let sum = 0;
-    for([key, value] of Object.entries(isOpenFingers)){
-        if (value){
-            sum = sum + Math.round(key);
-        }
-    }
-    return sum;
-}
-
+const notivication = document.getElementById('notification');
 const videoElement = document.getElementById('video');
 const canvasElement = document.getElementById('canvas');
 const canvasCtx = canvasElement.getContext('2d');
@@ -15,6 +6,7 @@ canvasCtx.translate(600, 0);
 canvasCtx.scale(-1, 1);
 const gestureElement = document.getElementById('gesture');
 
+const hand_location = localStorage.getItem('hands');
 const fingers = parseInt(localStorage.getItem('fingers'));
 const mode = localStorage.getItem('mode');
 const exercises = parseInt(localStorage.getItem('exercises'));
@@ -24,10 +16,10 @@ const hands = new Hands({locateFile: (file) => {
 }});
 
 hands.setOptions({
-    maxNumHands: 1,
+    maxNumHands: hand_location=="Multi"? 2 : 1,
     modelComplexity: 1,
     minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5
+    minTrackingConfidence: 0.5,
 });
 
 const exlist = [];
@@ -111,60 +103,71 @@ img.id = 'hand_image';
 imageContainer.appendChild(img);
 
 let iter = 0;
-hands.onResults(onResults);
 
-function onResults(results) {
-    canvasCtx.save();
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.drawImage(
-        results.image, 0, 0, canvasElement.width, canvasElement.height);
+// Обработка результатов
+hands.onResults((results) => {
+    // Очистка canvas
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Отрисовка видео на canvas
+    canvasCtx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
     if (results.multiHandLandmarks) {
-        for (const landmarks of results.multiHandLandmarks) {
-            drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
-                {color: '#00FF00', lineWidth: 5});
-            drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
+        results.multiHandLandmarks.forEach((landmarks, index) => {
+            // Определение левая или правая рука
+            const handedness = results.multiHandedness[index].label;
 
-            // Определение жестов
-            var isOpenFingers = {
-                16: landmarks[5].y > landmarks[4].y,    //thumb   10000
-                8: landmarks[6].y > landmarks[8].y,     //index   01000
-                4: landmarks[10].y > landmarks[12].y,   //middle  00100
-                2: landmarks[14].y > landmarks[16].y,   //ring    00010
-                1: landmarks[18].y > landmarks[20].y    //pinky   00001
-            };
+            console.log(hand_location)
+            // Отрисовка точек руки
+            drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+                color: handedness != hand_location ? '#00FF00' : '#FF0000',
+                lineWidth: 2
+            });
 
-            let sum = get_sum(isOpenFingers);
-            if(sum == exlist[iter] && iter<=exercises){
-                outputDivResult.innerHTML = "Упражнение закончено!";
-                iter++;
-                if (iter == exercises){
-                    outputDivResult.innerHTML = "Тренировка окончена!!!";
-                    exlist = [];
-                    iter = 0;
+            drawLandmarks(canvasCtx, landmarks, {
+                color: handedness != hand_location ? '#00FF00' : '#FF0000',
+                lineWidth: 1
+            });
+
+            if (handedness != hand_location){
+                // Определение жестов на одной руке
+                var isOpenFingers = {
+                    16: landmarks[5].y  > landmarks[4].y,    //thumb   10000
+                    8:  landmarks[6].y  > landmarks[8].y,    //index   01000
+                    4:  landmarks[10].y > landmarks[12].y,   //middle  00100
+                    2:  landmarks[14].y > landmarks[16].y,   //ring    00010
+                    1:  landmarks[18].y > landmarks[20].y    //pinky   00001
+                };
+
+                let sum = get_sum(isOpenFingers);
+                if(iter>=exercises){
+                    outputDivResult.innerHTML = "Тренировка окончена!";
+                    show_notification("Тренировка окончена!", 'base');
+                    setTimeout(() => {
+                        window.location.href = '/';
+                    }, 2000);
+                }
+                else if(sum == exlist[iter]){
+                    iter+=1;
                 }
                 else{
                     let binn = exlist[iter].toString(2);
                     binn = "00000".substr(binn.length) + binn;
-                    outputDivNextval.innerHTML = "Следующее упражнение : ";
-
-                    outputDivResult.innerHTML = " ";
-
                     let imagePath = 'http://127.0.0.1:8000/static/hand_gesture/img/'+exlist[iter]+'.jpg';
                     const imageContainer = document.getElementById('image-container');
                     const img = document.createElement('img');
                     img.src = imagePath;
                     img.id = 'hand_image';
-
                     const old_img = document.getElementById('hand_image');
                     imageContainer.replaceChild(img, old_img);
-
                 }
             }
-        }
+            else{
+                show_notification("Смените руку!", type='error');
+            }
+        });
     }
-    canvasCtx.restore();
-}
+});
 
 const camera = new Camera(videoElement, {
     onFrame: async () => {
@@ -174,3 +177,51 @@ const camera = new Camera(videoElement, {
     height: 480
 });
 camera.start();
+
+function get_sum(isOpenFingers){
+    let sum = 0;
+    for([key, value] of Object.entries(isOpenFingers)){
+        if (value){
+            sum = sum + Math.round(key);
+        }
+    }
+    return sum;
+}
+
+function show_notification(text, type){
+    // Уведомление через DOM
+    const notification = document.createElement('div');
+    notification.textContent = text;
+
+    if(type == 'base'){
+        notification.style.cssText = `
+        position: fixed;
+        top: 20px;
+        right: 20px;
+        padding: 15px;
+        background: #666370;
+        color: white;
+        border-radius: 5px;
+        box-shadow: 0 2px 5px 666370;
+        `;
+    }
+    if(type == 'error'){
+        notification.style.cssText = `
+        position: fixed;
+        top: 20px;
+        right: 20px;
+        padding: 15px;
+        background: #D33E43;
+        color: white;
+        border-radius: 5px;
+        box-shadow: 0 2px 5px D33E43;
+        `;
+    }
+
+    document.body.appendChild(notification);
+
+    // Автоматическое скрытие через 3 секунды
+    setTimeout(() => {
+        notification.remove();
+    }, 500);
+}
--- hand_gesture/urls.py
+++ hand_gesture/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ex_gesture, name='ex_gesture_page'),
-    path('video_feed/', views.analyze_emotion, name='video_feed')
+    # path('video_feed/', views.analyze_emotion, name='video_feed')
 ]
