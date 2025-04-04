0d65d0bb6e717f3e11f8f41925dcf91e0cbf01a8
Katherine Marakhova <40491346+KatherineMarakhova@users.noreply.github.com>
2025-04-04 21:30:46 +0300

    Update settings.py

--- profile-service/backend/backend/backend/settings.py
+++ profile-service/backend/backend/backend/settings.py
@@ -28,12 +28,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 ALLOWED_HOSTS = ["localhost"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'katherine.marakhova@gmail.com'
-EMAIL_HOST_PASSWORD = 'Marakhova334'
+
 
 
 # Application definition
