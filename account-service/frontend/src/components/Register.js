68bed176e14c5aef237ba85520285034d474a4dc
murka <katherine.marakhova@gmail.com>
2025-05-05 11:31:01 +0300

    добавлена вадлидация форм входа, регистрации, сброса пароля

--- account-service/frontend/src/components/Register.js
+++ account-service/frontend/src/components/Register.js
@@ -4,8 +4,11 @@ import { register } from '../api';
 
 export default function Register() {
   const [form, setForm] = useState({ username: '', email: '', password: '', password_check: '' });
-  const [paswordError, setpaswordError] = useState('');
+  const [paswordError, setPaswordError] = useState('');
+  const [emailError, setEmailError] = useState('');
   const navigate = useNavigate();
+  const reEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const rePassw = /^(?=.*?[0-9])(?=.*?[A-Za-z]).{8,32}$/;
 
   const handleChange = e => setForm({ ...form, [e.target.name]: e.target.value });
 
@@ -14,14 +17,18 @@ export default function Register() {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
+      setEmailError(reEmail.test(form.email)?"":"Некорректный email")
+      if (rePassw.test(form.password) && form.password.length >= 8 && reEmail.test(form.email)) {
         if (form.password_check === form.password) {
-            await register(form.username, form.email, form.password);
-            alert('Регистрация успешна!');
-            navigate("/");
+          await register(form.username, form.email, form.password);
+          alert('Регистрация успешна!');
+          navigate("/");
         } else {
-            setpaswordError("Пароли не совпадают")
+          setPaswordError("Пароли не совпадают")
         }
-      
+      } else {
+        setPaswordError("Пароль должен содержать как минимум 8 симфолов, одну заглавную букву и цифру")
+      }
     } catch {
       alert('Ошибка регистрации');
     }
@@ -29,7 +36,8 @@ export default function Register() {
 
   return (
     <div className="login-form">
-      <h2> Регистрация </h2> 
+      <a href="/" className="link">Войти</a>
+      <h2> Регистрация </h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label className="form-label"> Логин </label>
@@ -38,13 +46,14 @@ export default function Register() {
         <div className="form-group">
           <label className="form-label"> Email </label>
           <input name="email" className="form-control" onChange={handleChange} required />
+          <div style={{color:'#D33F49'}}>{ emailError }</div>
         </div>
         <div className="form-group">
           <label className="form-label"> Пароль </label>
           <input name="password" className="form-control" type="password" onChange={handleChange} required />
+          <div style={{color:'#D33F49'}}>{ paswordError }</div>
         </div>
         <div className="form-group">
-          <div style={{color:'#D33F49'}}>{ paswordError }</div>
           <label className="form-label"> Повторите пароль </label>
           <input name="password_check" className="form-control" type="password" onChange={handleChange} required />
         </div>
